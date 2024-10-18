024b5e4bb9139e04e36d199555e9ca05f0178a18
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-10-18 21:04:42 +0700

    Initial commit

---
+++ src/main/java/com/jelwery/morri/MorriApplication.java
@@ -0,0 +1,13 @@
+package com.jelwery.morri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jelwery/morri/MorriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jelwery.morri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
